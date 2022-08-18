16e3af3ff459855e8ffefb8617b9d376278eabb5
samlarson94 <samlarson94@gmail.com>
2022-08-18 13:42:44 -0500

    Setting up express server.js

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+//Import express, cors, and restaurants route file
+import express from 'express'
+import cors from "cors"
+import restaurants from "./api/restaurants.route.js"
+
+//Set up express server
+const app = express()
+
+//Add Middleware - Cors module
+app.use(cors())
+//Set up express.json to allow server to read json requests
+app.use(express.json())
+
+//Set up initial routes - others in routes folder
+app.use("api/v1/restaurants", restaurants)
+//Redirect route - * to signify any other route thats not established
+app.use("*", (req, res) => res.status(404).json({error: "oh dear, this route could not be found!"}))
+
+//Export app as a module
+export default app
